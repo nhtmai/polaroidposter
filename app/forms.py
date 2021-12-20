f055ff0f3520c7bbd24a9a1210a6c9ba9e5fcf56
maing <mainhatng@gmail.com>
2021-12-20 16:37:30 -0500

    debugging

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
-from .flask_wtf import FlaskForm
-from .wtforms import StringField
-from .wtforms.fields.html5 import URLField
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields.html5 import URLField
 
 
 class SearchForm(FlaskForm):
